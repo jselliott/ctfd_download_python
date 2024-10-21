2f2edec5b549a70002b0377082cebf3c49136cb4
alphillips-lab <52300472+alphillips-lab@users.noreply.github.com>
2024-10-21 19:24:53 -0400

    Switch to argparse, add capabilty to update an existing path with new challenges rather than redownloading all of the challenges

--- download.py
+++ download.py
@@ -2,12 +2,12 @@
 import requests
 import json
 import logging
-import sys
-import getopt
 import os
 from urllib.parse import urljoin, urlparse
 import re
 from tqdm import tqdm
+import argparse
+import textwrap
 
 logging.basicConfig()
 logging.root.setLevel(logging.INFO)
@@ -20,185 +20,191 @@ def slugify(text):
     text = re.sub(r"^-|-$", "", text)
     return text
 
+def _get_args():
+    parser = argparse.ArgumentParser(
+      prog='CTFd Download Tool',
+      formatter_class=argparse.RawDescriptionHelpFormatter,
+      epilog=textwrap.dedent('''\
+        Extra Information:
+            This tool can be used to download CTFd instances, particularly for generating writeups.
+        Example Usage:
+            python download.py -u http://myctf.ctfd.io/ -n MyCTF -o /tmp/myctf-writeups -t api_token
+         '''))
+    parser.add_argument("-u", "--url", help="CTF Base URL (http://myctf.ctfd.io/)", required=True)
+    parser.add_argument("-n", "--name", help="CTF Name (MyCTF)", required=True)
+    parser.add_argument("-t", "-c", "-s", "--session", help="API Token or Session Cookie (cookie format session=<SESSION_TOKEN>)", required=True)
+    parser.add_argument("-o", "--output", help="Output Directory", required=True)
+    parser.add_argument("--update", help="Only pull challenges that don't currently have a directory in your repository", action="store_true")
+    return parser.parse_args()
 
-def main(argv):
-    options = ('python download.py\n'
-               '-u (CTF URL)\n'
-               '-n (CTF name)\n'
-               '-o (CTF output directory)\n'
-               '-t (API token) OR -c (Cookie: session=?)\n\n'
-               'e.g python download.py -u http://ctf.url -n ctf_name -o /home/user/Desktop/ -t api_token')
-    try:
-        opts, _ = getopt.getopt(argv, 'hu:n:o:t:c:', ['help', 'url=', 'name=', 'output=', 'token=', 'cookie='])
-    except getopt.GetoptError:
-        print('python download.py -h')
-        sys.exit(2)
-    if len(opts) < 4:
-        print(options)
-        sys.exit()
-    if '-h' in opts or '--help' in opts:
-        print(options)
-        sys.exit()
+
+def main():
+    args = _get_args()
+    headers = {"Content-Type": "application/json"}
+    baseUrl = args.url
+    ctfName = args.name
+    outputDir = args.output
+    if args.session.startswith("session="):
+        headers["Cookie"] = args.session
     else:
-        baseUrl, ctfName, outputDir, = "", "", ""  # defaults?
-        headers = {"Content-Type": "application/json"}
-        for opt, arg in opts:
-            if opt in ('-u', '--url'):
-                baseUrl = arg  # URL of the CTFd
-            if opt in ('-n', '--name'):
-                ctfName = arg  # CTFd Name
-            if opt in ('-o', '--output'):
-                outputDir = arg  # Local directory to output docs
-            if opt in ('-t', '--token'):
-                headers["Authorization"] = f"Token {arg}"  # CTFd API Token
-            elif opt in ('-c', '--cookie'):
-                headers["Cookie"] = f"session={arg}"  # CTFd API Token
+        headers["Authorization"] = args.session
+    
+    # Add the API endpoint to the baseURL
+    apiUrl = urljoin(baseUrl, '/api/v1')
 
-        os.makedirs(outputDir, exist_ok=True)
+    # Make the output directory
+    os.makedirs(outputDir, exist_ok=True)
 
-        for d in ["challenges", "images"]:
-            os.makedirs(os.path.join(outputDir, d), exist_ok=True)
+    # Make directories for challenges and images
+    for d in ["challenges", "images"]:
+        os.makedirs(os.path.join(outputDir, d), exist_ok=True)
 
-        apiUrl = urljoin(baseUrl, '/api/v1')
+    logging.info("Connecting to API: %s" % apiUrl)
 
-        logging.info("Connecting to API: %s" % apiUrl)
+    S = requests.Session()
+    X = S.get(f"{apiUrl}/challenges", headers=headers).text
 
-        S = requests.Session()
-        X = S.get(f"{apiUrl}/challenges", headers=headers).text
+    challenges = json.loads(X)
 
-        challs = json.loads(X)
+    # Verify that we actually got a list of challenges
+    try:
+        logging.info("Retrieved %d challenges..." % len(challenges['data']))
+    except:
+        logging.fatal(challenges)
+        exit()
 
-        categories = {}
+    categories = {}
+    desc_links = []
 
-        try:
-            logging.info("Retrieved %d challenges..." % len(challs['data']))
-        except:
-            logging.fatal(challs)
-            exit()
+    for chall in challenges['data']:
 
-        desc_links = []
+        Y = json.loads(S.get(f"{apiUrl}/challenges/{chall['id']}", headers=headers).text)["data"]
 
-        for chall in challs['data']:
+        if Y["category"] not in categories:
+            categories[Y["category"]] = [Y]
+        else:
+            categories[Y["category"]].append(Y)
 
-            Y = json.loads(S.get(f"{apiUrl}/challenges/{chall['id']}", headers=headers).text)["data"]
+        catDir = os.path.join(outputDir, "challenges", Y["category"])
+        challDir = os.path.join(catDir, slugify(Y["name"]))
 
-            if Y["category"] not in categories:
-                categories[Y["category"]] = [Y]
-            else:
-                categories[Y["category"]].append(Y)
-
-            catDir = os.path.join(outputDir, "challenges", Y["category"])
-            challDir = os.path.join(catDir, slugify(Y["name"]))
-
+        os.makedirs(catDir, exist_ok=True)
+        if not args.update:
             os.makedirs(challDir, exist_ok=True)
-            os.makedirs(catDir, exist_ok=True)
+        else:
+            try:
+                os.makedirs(challDir)
+            except FileExistsError:
+                logging.warning("Skipping download for %s" % challDir)
+                continue
 
-            with open(os.path.join(challDir, "README.md"), "w") as chall_readme:
-                logging.info("Creating challenge readme: %s" % Y["name"])
-                chall_readme.write("# %s\n\n" % Y["name"])
-                chall_readme.write("## Description\n\n%s\n\n" % Y["description"])
+        with open(os.path.join(challDir, "README.md"), "w") as chall_readme:
+            logging.info("Creating challenge readme: %s" % Y["name"])
+            chall_readme.write("# %s\n\n" % Y["name"])
+            chall_readme.write("## Description\n\n%s\n\n" % Y["description"])
 
-                files_header = False
+            files_header = False
 
-                # Find links in description
-                links = re.findall(r'(https?://[^\s]+)', Y["description"])
+            # Find links in description
+            links = re.findall(r'(https?://[^\s]+)', Y["description"])
 
-                if len(links) > 0:
-                    for link in links:
-                        desc_links.append((Y["name"], link))
+            if len(links) > 0:
+                for link in links:
+                    desc_links.append((Y["name"], link))
 
-                # Find MD images in description
-                md_links = re.findall(r'!\[(.*)\]\(([^\s]+)\)', Y["description"])
+            # Find MD images in description
+            md_links = re.findall(r'!\[(.*)\]\(([^\s]+)\)', Y["description"])
 
-                if len(md_links) > 0:
-                    for link_desc, link in md_links:
-                        dl_url = urljoin(baseUrl, link)
+            if len(md_links) > 0:
+                for link_desc, link in md_links:
+                    dl_url = urljoin(baseUrl, link)
 
-                        F = S.get(dl_url, stream=True)
+                    F = S.get(dl_url, stream=True)
 
-                        fname = urlparse(f_url).path.split("/")[-1]
+                    fname = urlparse(f_url).path.split("/")[-1]
 
-                        if link[0] in ["/", "\\"]:
-                            link = link[1:]
+                    if link[0] in ["/", "\\"]:
+                        link = link[1:]
 
-                        local_f_path = os.path.join(outputDir, link)
-                        os.makedirs(os.path.join(outputDir, os.path.dirname(link)), exist_ok=True)
+                    local_f_path = os.path.join(outputDir, link)
+                    os.makedirs(os.path.join(outputDir, os.path.dirname(link)), exist_ok=True)
 
-                        total_size_in_bytes = int(F.headers.get('content-length', 0))
-                        progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True, desc=fname)
+                    total_size_in_bytes = int(F.headers.get('content-length', 0))
+                    progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True, desc=fname)
 
-                        with open(local_f_path, "wb") as LF:
-                            for chunk in F.iter_content(chunk_size=1024):
-                                if chunk:
-                                    progress_bar.update(len(chunk))
-                                    LF.write(chunk)
-                            LF.close()
+                    with open(local_f_path, "wb") as LF:
+                        for chunk in F.iter_content(chunk_size=1024):
+                            if chunk:
+                                progress_bar.update(len(chunk))
+                                LF.write(chunk)
+                        LF.close()
 
-                        progress_bar.close()
+                    progress_bar.close()
 
-                if "files" in Y and len(Y["files"]) > 0:
+            if "files" in Y and len(Y["files"]) > 0:
 
-                    if not files_header:
-                        chall_readme.write("## Files\n\n")
+                if not files_header:
+                    chall_readme.write("## Files\n\n")
 
-                    challFiles = os.path.join(challDir, "files")
-                    os.makedirs(challFiles, exist_ok=True)
+                challFiles = os.path.join(challDir, "files")
+                os.makedirs(challFiles, exist_ok=True)
 
-                    for file in Y["files"]:
+                for file in Y["files"]:
 
-                        # Fetch file from remote server
-                        f_url = urljoin(baseUrl, file)
-                        F = S.get(f_url, stream=True)
+                    # Fetch file from remote server
+                    f_url = urljoin(baseUrl, file)
+                    F = S.get(f_url, stream=True)
 
-                        fname = urlparse(f_url).path.split("/")[-1]
-                        local_f_path = os.path.join(challFiles, fname)
+                    fname = urlparse(f_url).path.split("/")[-1]
+                    local_f_path = os.path.join(challFiles, fname)
 
-                        chall_readme.write("* [%s](<files/%s>)\n\n" % (fname, fname))
+                    chall_readme.write("* [%s](<files/%s>)\n\n" % (fname, fname))
 
-                        total_size_in_bytes = int(F.headers.get('content-length', 0))
-                        progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True, desc=fname)
+                    total_size_in_bytes = int(F.headers.get('content-length', 0))
+                    progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True, desc=fname)
 
-                        with open(local_f_path, "wb") as LF:
-                            for chunk in F.iter_content(chunk_size=1024):
-                                if chunk:
-                                    progress_bar.update(len(chunk))
-                                    LF.write(chunk)
-                            LF.close()
+                    with open(local_f_path, "wb") as LF:
+                        for chunk in F.iter_content(chunk_size=1024):
+                            if chunk:
+                                progress_bar.update(len(chunk))
+                                LF.write(chunk)
+                        LF.close()
 
-                        progress_bar.close()
+                    progress_bar.close()
 
-                chall_readme.close()
+            chall_readme.close()
 
-        with open(os.path.join(outputDir, "README.md"), "w") as ctf_readme:
+    with open(os.path.join(outputDir, "README.md"), "w") as ctf_readme:
 
-            logging.info("Writing main CTF readme...")
+        logging.info("Writing main CTF readme...")
 
-            ctf_readme.write("# %s\n\n" % ctfName)
-            ctf_readme.write("## About\n\n[insert description here]\n\n")
-            ctf_readme.write("## Challenges\n\n")
+        ctf_readme.write("# %s\n\n" % ctfName)
+        ctf_readme.write("## About\n\n[insert description here]\n\n")
+        ctf_readme.write("## Challenges\n\n")
 
-            for category in categories:
-                ctf_readme.write("### %s\n\n" % category)
+        for category in categories:
+            ctf_readme.write("### %s\n\n" % category)
 
-                for chall in categories[category]:
+            for chall in categories[category]:
 
-                    chall_path = "challenges/%s/%s/" % (chall['category'], slugify(chall['name']))
-                    ctf_readme.write("* [%s](<%s>)" % (chall['name'], chall_path))
+                chall_path = "challenges/%s/%s/" % (chall['category'], slugify(chall['name']))
+                ctf_readme.write("* [%s](<%s>)" % (chall['name'], chall_path))
 
-                    if "tags" in chall and len(chall["tags"]) > 0:
-                        ctf_readme.write(" <em>(%s)</em>" % ",".join(chall["tags"]))
+                if "tags" in chall and len(chall["tags"]) > 0:
+                    ctf_readme.write(" <em>(%s)</em>" % ",".join(chall["tags"]))
 
-                    ctf_readme.write("\n")
+                ctf_readme.write("\n")
+            ctf_readme.write("\n")
 
-            ctf_readme.close()
+        ctf_readme.close()
 
-        logging.info("All done!")
+    logging.info("All done!")
 
-        if len(desc_links) > 0:
-            logging.warning("** Warning, the following links were found in challenge descriptions, you may need to download these files manually.")
-            for cname, link in desc_links:
-                logging.warning("%s - %s" % (cname, link))
+    if len(desc_links) > 0:
+        logging.warning("** Warning, the following links were found in challenge descriptions, you may need to download these files manually.")
+        for cname, link in desc_links:
+            logging.warning("%s - %s" % (cname, link))
 
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main()
